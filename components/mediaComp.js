562bcb38b22f34270b41c7b2a7add170fa507e2c
Braeden <braeden.craig@gmail.com>
2020-02-02 11:04:48 -0500

    masonry style

--- components/mediaComp.js
+++ components/mediaComp.js
@@ -192,7 +192,9 @@ export default class About extends React.Component {
     return (
       <div className='wrapper'>
         <div className='title'>
-          <h3>PHOTOS AND VIDEOS</h3>
+          <h3>
+            PHOTOS AND VIDEOS do masonry style layout for photos and videos
+          </h3>
         </div>
         <div className='flexIt'>
           {this.state.media.length === 0 ? (
